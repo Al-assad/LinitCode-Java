13633fd48ea64b3e316cbcb874269d0d9d276c5c
Al-assad <yulinying_1994@outlook.com>
2018-04-19 21:16:25 +0800

    updte

--- src/cowcoder/Solution9.java
+++ src/cowcoder/Solution9.java
@@ -9,6 +9,12 @@ package cowcoder;
  */
 public class Solution9 {
     public int NumberOf1(int n) {
-
+        // return Integer.bitCount(n);
+        n = n - ((n >>> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >>> 2) & 0x33333333);
+        n = (n + (n >>> 4)) & 0x0f0f0f0f;
+        n = n + (n >>> 8);
+        n = n + (n >>> 16);
+        return n & 0x3f;
     }
 }
